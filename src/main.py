88d07bae09f7154ab11c6f118d02d6ce7796e965
jhersara <linerslink@gmail.com>
2025-08-28 23:18:12 -0500

    Cambios en main para despliege en render.com

--- src/main.py
+++ src/main.py
@@ -45,4 +45,5 @@ def serve(path):
 if __name__ == '__main__':
     import os
     port = int(os.environ.get('PORT', 5001))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    #app.run(host='0.0.0.0', port=port, debug=False)
+    app.run()
